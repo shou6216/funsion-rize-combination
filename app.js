da8f665f941653e684cd3b47e5aef0fd0d22474d
Sho Yoshino <shou-y6216@yoshinoda.com>
2017-12-09 23:37:43 +0900

    [add] monsterのpath

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var ultra = require('./routes/ultra');
-var users = require('./routes/users');
+var monster = require('./routes/monster');
 
 var app = express();
 
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/index', index);
 app.use('/ultra', ultra);
-app.use('/users', users);
+app.use('/monster', monster);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
