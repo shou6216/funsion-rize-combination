c9ba19670e4572c983953c89d2e930c06a610a13
Sho Yoshino <shou-y6216@yoshinoda.com>
2017-12-12 00:36:28 +0900

    [add] Tab追加しました

--- routes/list.js
+++ routes/list.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('list', { title: 'ウルトラマン' });
+  res.render('list', { title: 'カプセル一覧' });
 });
 
 module.exports = router;
