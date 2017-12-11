815586fe7e9b2685c161c8c00cf7a51280fca5b3
Sho Yoshino <shou-y6216@yoshinoda.com>
2017-12-11 22:27:12 +0900

    [update] indexをindex.jsにまとめました。

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/index', index);
 app.use('/ultra', ultra);
 app.use('/monster', monster);
 
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', redirectTop);
+router.get('/index', redirectTop);
+
+function redirectTop(req, res, next) {
   res.redirect(301, "/ultra");
-});
+}
 
 module.exports = router;
