25b5cc2f7f4ae047c275b3410d819ac4f90affe3
Sho Yoshino <shou-y6216@yoshinoda.com>
2017-12-18 00:02:19 +0900

    [update] ウルトラマンと怪獣のデータをcontrollerから渡せるようにしました

--- routes/list.js
+++ routes/list.js
@@ -1,9 +1,56 @@
 var express = require('express');
 var router = express.Router();
 
+var ultraList = [
+  {
+    id : "U100",
+    fullName: "ウルトラマンジード",
+    shortName: "ジード",
+    stage: 1
+  },
+  {
+    id : "U101",
+    fullName: "ウルトラマンオーブ",
+    shortName: "オーブ",
+    stage: 2
+  },
+  {
+    id : "U102",
+    fullName: "ウルトラマンエックス",
+    shortName: "エックス",
+    stage: 3
+  }
+]
+
+var monsterList = [
+  {
+    id : "M100",
+    fullName: "ゴモラ",
+    shortName: "ゴモラ",
+    stage: 1
+  },
+  {
+    id : "M101",
+    fullName: "ゼットン",
+    shortName: "ゼットン",
+    stage: 2
+  },
+  {
+    id : "M102",
+    fullName: "バードン",
+    shortName: "バードン",
+    stage: 3
+  }
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('list', { title: 'カプセル一覧' });
+  console.log(ultraList);
+  res.render('list', { 
+    title: 'カプセル一覧',
+    ultraList: ultraList,
+    monsterList: monsterList
+  });
 });
 
 module.exports = router;
