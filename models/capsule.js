4d2eac3e4a7320825263406830f0e0ebbf2ab460
Sho Yoshino <shou-y6216@yoshinoda.com>
2018-01-12 07:48:05 +0900

    [update] DBのリストを表示ウルトラと怪獣で分けて表示できるようにしました

---
+++ models/capsule.js
@@ -0,0 +1,7 @@
+var Capsule = function(id, fullName, nickName) {
+  this.id = id;
+  this.fullName = fullName;
+  this.nickName = nickName;
+}
+
+module.exports = Capsule;
--- routes/list.js
+++ routes/list.js
@@ -1,60 +1,26 @@
 var express = require('express');
 var router = express.Router();
-
 var sqlite3 = require('sqlite3');
 var db = new sqlite3.Database('fusion-rize.db');
-
-var ultraList = [
-  {
-    id : "U100",
-    fullName: "ウルトラマンジード",
-    shortName: "ジード",
-    stage: 1
-  },
-  {
-    id : "U101",
-    fullName: "ウルトラマンオーブ",
-    shortName: "オーブ",
-    stage: 2
-  },
-  {
-    id : "U102",
-    fullName: "ウルトラマンエックス",
-    shortName: "エックス",
-    stage: 3
-  }
-]
-
-var monsterList = [
-  {
-    id : "M100",
-    fullName: "ゴモラ",
-    shortName: "ゴモラ",
-    stage: 10
-  },
-  {
-    id : "M101",
-    fullName: "ゼットン",
-    shortName: "ゼットン",
-    stage: 20
-  },
-  {
-    id : "M102",
-    fullName: "バードン",
-    shortName: "バードン",
-    stage: 30
-  }
-]
+var Capsule = require('../models/capsule');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   db.serialize(() => {
     db.all("select * from capsule", (err, rows) => {
       if (!err) {
+        var ultraCapsuleList = rows.filter(function (row) {
+          return row.type === 'ultra';
+        })
+
+        var monsterCapsuleList = rows.filter(function (row) {
+          return row.type === 'monster';
+        })
+
         res.render('list', { 
           title: 'カプセル一覧',
-          ultraList: rows,
-          monsterList: monsterList
+          ultraList: ultraCapsuleList,
+          monsterList: monsterCapsuleList
         });      
       }
     });
