e7852ba414c7dbef93c2c4aad384be05f7fc3cfe
Sho Yoshino <shou-y6216@yoshinoda.com>
2018-01-14 22:49:46 +0900

    [update] 詳細画面を一覧画面をベースにへんこうしました

--- routes/detail.js
+++ routes/detail.js
@@ -4,7 +4,11 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/:capsuleId(\\d+)', function(req, res, next) {
   console.log(req.params.capsuleId);
-  res.render('detail', { title: '怪獣' });
+  res.render('detail', { 
+    title: 'カプセル詳細',
+    ultraList: [],
+    monsterList: []
+  }); 
 });
 
 module.exports = router;
