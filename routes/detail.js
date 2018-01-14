a7cb8d7d73fdc106a53bfea741c3271a9ea9966a
Sho Yoshino <shou-y6216@yoshinoda.com>
2018-01-14 23:52:22 +0900

    [update] mappingから指定のcapsuleIdで取得できるようにしました

--- routes/detail.js
+++ routes/detail.js
@@ -1,14 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('knex')({
+  dialect: 'sqlite3',
+  connection: {
+    filename: 'fusion-rize.db'
+  },
+  useNullAsDefault: true
+});
+var Bookshelf = require('bookshelf')(knex);
+var DBMapping = Bookshelf.Model.extend({
+  tableName: 'mapping'
+})
 
 /* GET users listing. */
 router.get('/:capsuleId(\\d+)', function(req, res, next) {
-  console.log(req.params.capsuleId);
-  res.render('detail', { 
-    title: 'カプセル詳細',
-    ultraList: [],
-    monsterList: []
-  }); 
+  new DBMapping().where('youCapsuleId', '=', req.params.capsuleId)
+    .fetchAll()
+    .then((collection) => {
+      var mappings = collection.toArray().map(function(col) {
+        return col.attributes;
+      });
+      console.log(mappings);
+
+      res.render('detail', { 
+        title: 'カプセル詳細',
+        ultraList: [],
+        monsterList: []
+      });     
+
+  }).catch((err) =>{
+    next(err);
+  });
 });
 
 module.exports = router;
