86d2051392740f8ecece37d59be99b333bfdac19
Sho Yoshino <shou-y6216@yoshinoda.com>
2018-01-21 02:36:10 +0900

    [update] detail.jsでsqliteClient.js使うようにしました

--- routes/detail.js
+++ routes/detail.js
@@ -1,43 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var knex = require('knex')({
-  dialect: 'sqlite3',
-  connection: {
-    filename: 'fusion-rize.db'
-  },
-  useNullAsDefault: true
-});
-var Bookshelf = require('bookshelf')(knex);
+var sqliteClient = require('../models/sqliteClient');
 var ViewFusion = require('../models/fusion');
-var DBMapping = Bookshelf.Model.extend({
-  tableName: 'mapping',
-  iCapsule: function() {
-    return this.belongsTo(DBCapsule, 'iCapsuleId', 'id');
-  },
-  fusion: function() {
-    return this.belongsTo(DBFusion, 'fusionId', 'id');
-  }
-})
-var DBCapsule = Bookshelf.Model.extend({
-  tableName: 'capsule',
-  mapping: function() {
-    return this.hasOne(DBMapping);
-  }
-})
-var DBFusion = Bookshelf.Model.extend({
-  tableName: 'fusion',
-  mapping: function() {
-    return this.hasOne(DBMapping);
-  }
-})
 
 router.get('/:capsuleId(\\d+)', function(req, res, next) {
-  new DBCapsule()
+  new sqliteClient.Capsule()
     .where('id', '=', req.params.capsuleId)
     .fetch({require: true})
     .then((col) => {
       var youCapsule = col.toJSON();
-      new DBMapping()
+      new sqliteClient.Mapping()
         .where('youCapsuleId', '=', youCapsule.id)
         .fetchAll({withRelated: ['iCapsule', 'fusion']})
         .then((collection) => {
